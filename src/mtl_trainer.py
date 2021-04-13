3b80822187798aaf230ee50e841e07efbe2b1a0e
heatxg <heat.grebe.763@gmail.com>
2021-04-13 11:34:58 -0700

    first pass at integrating logging to wandb for each individual task

--- src/mtl_trainer.py
+++ src/mtl_trainer.py
@@ -14,13 +14,19 @@ from transformers import (
     TrainingArguments, 
     EvalPrediction, 
     DataCollator,
-    DefaultDataCollator
+    DefaultDataCollator,
+    PredictionOutput
 )
 
 from src.data.task_data_processors import task_output_modes
 
 from src.data.data_utils import compute_task_metrics
 
+if is_tpu_available():
+    import torch_xla.core.xla_model as xm
+    import torch_xla.debug.metrics as met
+    import torch_xla.distributed.parallel_loader as pl
+    
 logger = logging.getLogger(__name__)
 
 
@@ -202,11 +208,22 @@ class MultiTaskTrainer(Trainer):
         for task_name, eval_dataset in datasets.items():
             logger.info(task_name)
             self.compute_metrics = self.build_compute_metrics_fn(eval_dataset)
-            eval_result = super().evaluate(
-                eval_dataset=eval_dataset, prediction_loss_only=True
-            )
+            eval_dataloader = self.get_eval_dataloader(eval_dataset)
+            
+            eval_result = self._prediction_loop(
+                eval_dataloader, description="Evaluation", task_name=task_name, 
+                mode=test_dataset.mode)
+            
+            self._log(eval_result.metrics)
+
             for key, value in eval_result.items():
                 logger.info("  %s = %s", key, value)
+
+        if self.args.tpu_metrics_debug:
+            # tpu-comment: Logging debug metrics for PyTorch/XLA (compile, execute times, ops, etc.)
+            xm.master_print(met.metrics_report())
+
+        return output.metrics
 
     def predict(
         self,
@@ -217,7 +234,17 @@ class MultiTaskTrainer(Trainer):
         datasets = eval_dataset or self.test_datasets
         for task_name, test_dataset in datasets.items():
             logger.info(task_name)
-            predictions = super().predict(test_dataset=test_dataset).predictions
+            
+            test_dataloader = self.get_test_dataloader(test_dataset)
+            test_result = self._prediction_loop(
+                test_dataloader, description="Prediction", task_name=task_name, 
+                mode=test_dataset.mode)
+            
+            self._log(test_result.metrics)
+            for key, value in eval_result.items():
+                logger.info("  %s = %s", key, value)
+                
+            predictions = test_result.predictions
             output_mode = task_output_modes[task_name] 
             if output_mode == "classification":
                 predictions = np.argmax(predictions, axis=1)
@@ -237,7 +264,103 @@ class MultiTaskTrainer(Trainer):
                             writer.write(
                                 "%d\t%s\n" % (index, test_dataset.get_labels()[item])
                             )
+    def _prediction_loop(
+        self, dataloader: DataLoader, description: str, task_name: str, mode: str,
+        prediction_loss_only: Optional[bool] = None, 
+    ) -> PredictionOutput:
+        """
+        Prediction/evaluation loop, shared by `evaluate()` and `predict()`.
+        Works both with or without labels.
+        """
 
+        prediction_loss_only = prediction_loss_only if prediction_loss_only is not None else self.prediction_loss_only
+
+        model = self.model
+        # multi-gpu eval
+        if self.args.n_gpu > 1:
+            model = torch.nn.DataParallel(model)
+        else:
+            model = self.model
+        # Note: in torch.distributed mode, there's no point in wrapping the model
+        # inside a DistributedDataParallel as we'll be under `no_grad` anyways.
+
+        batch_size = dataloader.batch_size
+        logger.info("***** Running %s *****", description)
+        logger.info("  Num examples = %d", self.num_examples(dataloader))
+        logger.info("  Batch size = %d", batch_size)
+        eval_losses: List[float] = []
+        preds: torch.Tensor = None
+        label_ids: torch.Tensor = None
+        model.eval()
+
+        if is_tpu_available():
+            dataloader = pl.ParallelLoader(dataloader,
+                                           [self.args.device]).per_device_loader(self.args.device)
+
+        for inputs in tqdm(dataloader, desc=description):
+            has_labels = any(
+                inputs.get(k) is not None for k in ["labels", "lm_labels", "masked_lm_labels"])
+
+            for k, v in inputs.items():
+                inputs[k] = v.to(self.args.device)
+
+            with torch.no_grad():
+                outputs = model(**inputs)
+                if has_labels:
+                    step_eval_loss, logits = outputs[:2]
+                    eval_losses += [step_eval_loss.mean().item()]
+                else:
+                    logits = outputs[0]
+
+            if not prediction_loss_only:
+                if preds is None:
+                    preds = logits.detach()
+                else:
+                    preds = torch.cat((preds, logits.detach()), dim=0)
+                if inputs.get("labels") is not None:
+                    if label_ids is None:
+                        label_ids = inputs["labels"].detach()
+                    else:
+                        label_ids = torch.cat((label_ids, inputs["labels"].detach()), dim=0)
+
+        if self.args.local_rank != -1:
+            # In distributed mode, concatenate all results from all nodes:
+            if preds is not None:
+                preds = self.distributed_concat(preds,
+                                                num_total_examples=self.num_examples(dataloader))
+            if label_ids is not None:
+                label_ids = self.distributed_concat(label_ids,
+                                                    num_total_examples=self.num_examples(dataloader))
+        elif is_tpu_available():
+            # tpu-comment: Get all predictions and labels from all worker shards of eval dataset
+            if preds is not None:
+                preds = xm.mesh_reduce("eval_preds", preds, torch.cat)
+            if label_ids is not None:
+                label_ids = xm.mesh_reduce("eval_label_ids", label_ids, torch.cat)
+
+        # Finally, turn the aggregated tensors into numpy arrays.
+        if preds is not None:
+            preds = preds.cpu().numpy()
+        if label_ids is not None:
+            label_ids = label_ids.cpu().numpy()
+
+        if self.compute_metrics is not None and preds is not None and label_ids is not None:
+            metrics = self.compute_metrics(EvalPrediction(predictions=preds, label_ids=label_ids))
+        else:
+            metrics = {}
+        if len(eval_losses) > 0:
+            metrics[f"{task_name}_{mode}_loss"] = np.mean(eval_losses)
+
+        # Prefix all keys with {task_name}_eval_
+        for key in list(metrics.keys()):
+            if not key.startswith(f"{task_name}_{mode}_"):
+                metrics[f"{task_name}_{mode}_{key}"] = metrics.pop(key)
+
+        return PredictionOutput(predictions=preds, label_ids=label_ids, metrics=metrics)
+                            
+                            
+                       
+                            
     @staticmethod
     def build_compute_metrics_fn(
         eval_dataset
