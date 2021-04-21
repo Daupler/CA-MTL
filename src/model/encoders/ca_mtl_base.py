3058f87c53b9b8bbaba7d4cbe46069a1d56c7ddd
heatxg <heat.grebe.763@gmail.com>
2021-04-21 07:28:34 -0700

    bring BertEmbeddings class in directly rather than importing it

--- src/model/encoders/ca_mtl_base.py
+++ src/model/encoders/ca_mtl_base.py
@@ -2,7 +2,8 @@ import math
 import torch
 import torch.nn as nn
 from transformers.modeling_bert import (
-    BertEmbeddings,
+#     BertEmbeddings,
+    BertLayerNorm,
     BertPooler,
     BertPreTrainedModel,
     BertAttention,
@@ -377,6 +378,54 @@ class MyBertEncoder9(nn.Module):
             outputs = outputs + (all_attentions,)
         return outputs  # last-layer hidden state, (all hidden states), (all attentions)
 
+    
+    
+############################################################################################
+############################################################################################    
+############################################################################################    
+############################################################################################    
+class BertEmbeddings(nn.Module):
+    """Construct the embeddings from word, position and token_type embeddings.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+        # any TensorFlow checkpoint file
+        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+        if position_ids is None:
+            position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
+            position_ids = position_ids.unsqueeze(0).expand(input_shape)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + position_embeddings + token_type_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+############################################################################################
+############################################################################################    
+############################################################################################    
+############################################################################################    
 
 class CaMtlBaseEncoder(BertPreTrainedModel):
     r"""
