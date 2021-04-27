429b5ecab93c110adcebc3a70d0333252c882ff5
heatxg <heat.grebe.763@gmail.com>
2021-04-27 09:55:58 -0700

    torchscrip debugging: kwargs are not supported and BertEmbedding forward pass cannot be overwritten. mitigated by importing BertEmbeddings directly and passing in task_id as an explict argument. rerun models indicate that these have no effect on model outputs

--- src/model/encoders/ca_mtl_base.py
+++ src/model/encoders/ca_mtl_base.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 from transformers.modeling_bert import (
-#     BertEmbeddings,
+    BertEmbeddings,
     BertLayerNorm,
     BertPooler,
     BertPreTrainedModel,
@@ -384,44 +384,44 @@ class MyBertEncoder9(nn.Module):
 ############################################################################################    
 ############################################################################################    
 ############################################################################################    
-class BertEmbeddings(nn.Module):
-    """Construct the embeddings from word, position and token_type embeddings.
-    """
+# class BertEmbeddings(nn.Module):
+#     """Construct the embeddings from word, position and token_type embeddings.
+#     """
 
-    def __init__(self, config):
-        super().__init__()
-        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
-        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
-        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+#     def __init__(self, config):
+#         super().__init__()
+#         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
+#         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+#         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
-        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
-        # any TensorFlow checkpoint file
-        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+#         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+#         # any TensorFlow checkpoint file
+#         self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
-        if input_ids is not None:
-            input_shape = input_ids.size()
-        else:
-            input_shape = inputs_embeds.size()[:-1]
+#     def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
+#         if input_ids is not None:
+#             input_shape = input_ids.size()
+#         else:
+#             input_shape = inputs_embeds.size()[:-1]
 
-        seq_length = input_shape[1]
-        device = input_ids.device if input_ids is not None else inputs_embeds.device
-        if position_ids is None:
-            position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
-            position_ids = position_ids.unsqueeze(0).expand(input_shape)
-        if token_type_ids is None:
-            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+#         seq_length = input_shape[1]
+#         device = input_ids.device if input_ids is not None else inputs_embeds.device
+#         if position_ids is None:
+#             position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
+#             position_ids = position_ids.unsqueeze(0).expand(input_shape)
+#         if token_type_ids is None:
+#             token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
 
-        if inputs_embeds is None:
-            inputs_embeds = self.word_embeddings(input_ids)
-        position_embeddings = self.position_embeddings(position_ids)
-        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+#         if inputs_embeds is None:
+#             inputs_embeds = self.word_embeddings(input_ids)
+#         position_embeddings = self.position_embeddings(position_ids)
+#         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
-        embeddings = inputs_embeds + position_embeddings + token_type_embeddings
-        embeddings = self.LayerNorm(embeddings)
-        embeddings = self.dropout(embeddings)
-        return embeddings
+#         embeddings = inputs_embeds + position_embeddings + token_type_embeddings
+#         embeddings = self.LayerNorm(embeddings)
+#         embeddings = self.dropout(embeddings)
+#         return embeddings
 ############################################################################################
 ############################################################################################    
 ############################################################################################    
@@ -463,7 +463,7 @@ class CaMtlBaseEncoder(BertPreTrainedModel):
 
     """
 
-    def __init__(self, config, data_args=None, **kwargs):
+    def __init__(self, config, data_args=None):
         super().__init__(config)
         tasks = data_args.tasks
         self.task_id_2_task_idx = {i: i for i, t in enumerate(tasks)}
@@ -505,7 +505,7 @@ class CaMtlBaseEncoder(BertPreTrainedModel):
         inputs_embeds=None,
         encoder_hidden_states=None,
         encoder_attention_mask=None,
-        **kwargs,
+        task_id=None,
     ):
         """Forward pass on the Model.
 
@@ -516,7 +516,7 @@ class CaMtlBaseEncoder(BertPreTrainedModel):
             https://arxiv.org/abs/1706.03762
 
         """
-        task_type = self._create_task_type(kwargs["task_id"])
+        task_type = self._create_task_type(task_id)
         task_embedding = self.task_type_embeddings(task_type)
 
         if input_ids is not None and inputs_embeds is not None:
