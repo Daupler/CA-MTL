542891bcbe47e5234ff4519028cff5528e62b523
heatxg <heat.grebe.763@gmail.com>
2021-04-27 12:07:55 -0700

    replacing default None type inputs to BertEmbedding forward pass. This is because TorchScript only supports static typing. To handle the logic based on the original None type, I replaced control statement arguments with logic that checks for an empty tensor and then defaults the forward pass argument to empty tensors. It is unlikely that this situation would ever be encountered in actual training but should be kept in mind just in case

--- src/model/encoders/ca_mtl_base.py
+++ src/model/encoders/ca_mtl_base.py
@@ -1,8 +1,9 @@
 import math
 import torch
 import torch.nn as nn
+from typing import List, Optional, Union
 from transformers.modeling_bert import (
-    BertEmbeddings,
+#     BertEmbeddings,
     BertLayerNorm,
     BertPooler,
     BertPreTrainedModel,
@@ -384,44 +385,48 @@ class MyBertEncoder9(nn.Module):
 ############################################################################################    
 ############################################################################################    
 ############################################################################################    
-# class BertEmbeddings(nn.Module):
-#     """Construct the embeddings from word, position and token_type embeddings.
-#     """
+class BertEmbeddings(nn.Module):
+    """Construct the embeddings from word, position and token_type embeddings.
+    """
 
-#     def __init__(self, config):
-#         super().__init__()
-#         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
-#         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
-#         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+    def __init__(self, config):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
-#         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
-#         # any TensorFlow checkpoint file
-#         self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+        # any TensorFlow checkpoint file
+        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-#     def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
-#         if input_ids is not None:
-#             input_shape = input_ids.size()
-#         else:
-#             input_shape = inputs_embeds.size()[:-1]
+    def forward(self, 
+                input_ids: torch.Tensor = torch.zeros(size=(1,0)), 
+                token_type_ids: torch.Tensor = torch.zeros(size=(1,0)),
+                position_ids: torch.Tensor = torch.zeros(size=(1,0)),
+                inputs_embeds: torch.Tensor = torch.zeros(size=(1,0))):
+        if not input_ids.size()[1] == 0:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
 
-#         seq_length = input_shape[1]
-#         device = input_ids.device if input_ids is not None else inputs_embeds.device
-#         if position_ids is None:
-#             position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
-#             position_ids = position_ids.unsqueeze(0).expand(input_shape)
-#         if token_type_ids is None:
-#             token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+        seq_length = input_shape[1]
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+        if not position_ids.size()[1] == 0:
+            position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
+            position_ids = position_ids.unsqueeze(0).expand(input_shape)
+        if not token_type_ids.size()[1] == 0:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
 
-#         if inputs_embeds is None:
-#             inputs_embeds = self.word_embeddings(input_ids)
-#         position_embeddings = self.position_embeddings(position_ids)
-#         token_type_embeddings = self.token_type_embeddings(token_type_ids)
+        if not inputs_embeds.size()[1] == 0:
+            inputs_embeds = self.word_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
-#         embeddings = inputs_embeds + position_embeddings + token_type_embeddings
-#         embeddings = self.LayerNorm(embeddings)
-#         embeddings = self.dropout(embeddings)
-#         return embeddings
+        embeddings = inputs_embeds + position_embeddings + token_type_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
 ############################################################################################
 ############################################################################################    
 ############################################################################################    
